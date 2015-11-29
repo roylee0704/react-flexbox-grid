49d606c50aac3cebb6db1db1f0cb495e2dc015b1
roylee0704 <roylee0704@gmail.com>
2015-11-29 18:55:12 +0800

    added styles on spec

--- spec/root.js
+++ spec/root.js
@@ -1,11 +1,24 @@
 /* global VERSION */
-require('./style');
+require('./stylesheets/base');
+import box from './stylesheets/modules/box';
 
 import React from 'react';
-import {Grid} from '../src/index';
+import {Grid, Row, Col} from '../src/index';
 
 const Root = () => (
-  <Grid>Hello World, Roy Lee</Grid>
+
+  <div>
+    <h1>React Flexbox Grid <small>Spec {VERSION}</small></h1>
+    <Grid>
+      <Row>
+        <Col xs="2">
+          <div className={box.row} />
+        </Col>
+      </Row>
+    </Grid>
+
+  </div>
+
 );
 
 
