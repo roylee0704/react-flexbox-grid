24e7a88285e1ed1d2b1cf5781fef24112ebec5b2
roylee0704 <roylee0704@gmail.com>
2015-12-03 17:25:24 +0800

    completed 1/3 milestones for spec

--- spec/root.js
+++ spec/root.js
@@ -6,19 +6,22 @@ import React from 'react';
 import {Grid, Row, Col} from '../src/index';
 
 const Root = () => (
-
   <div>
     <h1>React Flexbox Grid <small>Spec {VERSION}</small></h1>
     <Grid>
       <Row>
-        <Col xs="2">
+        <Col xs="12" sm="3" md="2" lg="1">
+          <div className={box.row} />
+        </Col>
+        <Col xs="6" sm="6" md="8" lg="10">
+          <div className={box.row} />
+        </Col>
+        <Col xs="6" sm="3" md="2" lg="1">
           <div className={box.row} />
         </Col>
       </Row>
     </Grid>
-
   </div>
-
 );
 
 
