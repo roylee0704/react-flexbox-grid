35fa5aca004c0f756b3e31a9e5de01f966017cbf
roylee0704 <roylee0704@gmail.com>
2015-12-08 23:53:25 +0800

    added webpack-dev for doc

---
+++ doc/app/index.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<div>Hehe</div>, document.getElementById('app'));
---
+++ doc/webpack.config.development.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const webpack = require('webpack');
+const autoprefixer = require('autoprefixer');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const TransferWebpackPlugin = require('transfer-webpack-plugin');
+
+module.exports = {
+  context: __dirname,
+  devtool: 'inline-source-map',
+  entry: [
+    'webpack-hot-middleware/client',
+    './app/index.jsx'
+  ],
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'docs.js',
+    publicPath: '/'
+  },
+  resolve: {
+    extensions: ['', '.jsx', '.scss', '.js', '.json', '.md'],
+    alias: {
+      'react-toolbox': path.resolve(__dirname + './../src')
+    },
+    modulesDirectories: [
+      'node_modules',
+      path.resolve(__dirname, './node_modules'),
+      path.resolve(__dirname, './../node_modules'),
+      path.resolve(__dirname, './../src')
+    ]
+  },
+  module: {
+    loaders: [
+      {
+        test: /(\.js|\.jsx)$/,
+        exclude: /(node_modules)/,
+        loader: 'babel'
+      }, {
+        test: /(\.scss|\.css)$/,
+        loader: ExtractTextPlugin.extract('style', 'css?sourceMap&modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss!sass?sourceMap!toolbox')
+      }, {
+        test: /(\.md)$/,
+        loader: 'html!highlight!markdown'
+      }
+    ]
+  },
+  postcss: [autoprefixer],
+  plugins: [
+    new ExtractTextPlugin('docs.css', { allChunks: true }),
+    new TransferWebpackPlugin([{
+      from: 'www/images',
+      to: 'images'
+    }], path.resolve(__dirname, './')),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('development')
+    })
+  ]
+};
