434b4888c97e1a43bf56b7eea3c21c748e0ce8df
roylee0704 <roylee0704@gmail.com>
2015-12-09 11:24:53 +0800

    initial component home component

---
+++ doc/app/components/home/index.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react'
+
+const Home = () => (
+  <article>
+    React FlexBox Grid : Work In Progress...
+  </article>
+);
+
+export default Home;
--- doc/app/index.jsx
+++ doc/app/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Home from './components/home';
 
-ReactDOM.render(<div>Hehe</div>, document.getElementById('app'));
+ReactDOM.render(<Home />, document.getElementById('app'));
