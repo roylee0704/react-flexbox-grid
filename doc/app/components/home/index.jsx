f83ac5f6c3317b46539e6ccdc84b3ebf97aa01eb
roylee0704 <roylee0704@gmail.com>
2015-12-15 20:10:46 +0800

    added component to read markdown

--- doc/app/components/home/index.jsx
+++ doc/app/components/home/index.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
-import {Row} from 'react-flexbox-grid';
+import {Grid, Row, Col} from 'react-flexbox-grid';
 import style from './style';
+
+import Markdown from '../markdown';
+import responsiveMD from './responsive';
 
 const Home = () => (
   <article>
@@ -15,6 +18,43 @@ const Home = () => (
         <a href="https://github.com/roylee0704/react-flexbox-grid">Github</a>
       </Row>
   </header>
+  <section>
+    <Grid fluid>
+      <h2>Responsive</h2>
+      <p>Responsive modifiers enable specifying different column sizes, offsets, alignment and distribution at xs, sm, md & lg viewport widths.</p>
+      <Row>
+        <Col xs={12} sm={3} md={2} lg={1}>
+          <div className={style.box} />
+        </Col>
+        <Col xs={6} sm={6} md={8} lg={10}>
+          <div className={style.box} />
+        </Col>
+        <Col xs={6} sm={3} md={2} lg={1}>
+          <div className={style.box} />
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={2} sm={3} md={2} lg={1}>
+          <div className={style.box} />
+        </Col>
+        <Col xs={2} sm={9} md={10} lg={11}>
+          <div className={style.box} />
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={10} sm={6} md={8} lg={10}>
+          <div className={style.box} />
+        </Col>
+        <Col xs={2} sm={6} md={4} lg={2}>
+          <div className={style.box} />
+        </Col>
+      </Row>
+
+        <Markdown html = {responsiveMD} />
+    </Grid>
+
+  </section>
+
   </article>
 );
 
