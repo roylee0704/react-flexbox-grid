6bf433946e519d90d28f86f14eb563c588fba404
roylee0704 <roylee0704@gmail.com>
2015-12-16 23:50:49 +0800

    refactored to composable-style

--- doc/app/components/home/index.jsx
+++ doc/app/components/home/index.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import {Grid, Row, Col} from 'react-flexbox-grid';
-import style from './style';
+import {Grid, Row} from 'react-flexbox-grid';
+import home from './style';
 
 import Markdown from '../markdown';
+import Section from '../section';
+import Box from '../box';
 import responsiveMD from './responsive';
 
 const Home = () => (
   <article>
     <header>
       <Row center="xs">
-        <h1 className={style.headline}>React-FlexBox-Grid</h1>
+        <h1 className={home.headline}>React-FlexBox-Grid</h1>
       </Row>
       <Row center="xs">
         <p>A react grid system based on flexboxgrid.css</p>
@@ -18,43 +20,27 @@ const Home = () => (
         <a href="https://github.com/roylee0704/react-flexbox-grid">Github</a>
       </Row>
   </header>
-  <section>
-    <Grid fluid>
-      <h2>Responsive</h2>
-      <p>Responsive modifiers enable specifying different column sizes, offsets, alignment and distribution at xs, sm, md & lg viewport widths.</p>
-      <Row>
-        <Col xs={12} sm={3} md={2} lg={1}>
-          <div className={style.box} />
-        </Col>
-        <Col xs={6} sm={6} md={8} lg={10}>
-          <div className={style.box} />
-        </Col>
-        <Col xs={6} sm={3} md={2} lg={1}>
-          <div className={style.box} />
-        </Col>
-      </Row>
-      <Row>
-        <Col xs={12} sm={3} md={2} lg={1}>
-          <div className={style.box} />
-        </Col>
-        <Col xs={12} sm={9} md={10} lg={11}>
-          <div className={style.box} />
-        </Col>
-      </Row>
-      <Row>
-        <Col xs={10} sm={6} md={8} lg={10}>
-          <div className={style.box} />
-        </Col>
-        <Col xs={2} sm={6} md={4} lg={2}>
-          <div className={style.box} />
-        </Col>
-      </Row>
-
-        <Markdown html = {responsiveMD} />
-    </Grid>
-
-  </section>
-
+  <Grid fluid className={home.wrap}>
+    <Section
+      title="Responsive"
+      description="Responsive modifiers enable specifying different column sizes, offsets, alignment and distribution at xs, sm, md & lg viewport widths." />
+    <Row>
+      <Box xs={12} sm={3} md={2} lg={1} />
+      <Box xs={6} sm={6} md={8} lg={10} />
+      <Box xs={6} sm={3} md={2} lg={1} />
+    </Row>
+    <Row>
+      <Box xs={12} sm={3} md={2} lg={1} />
+      <Box xs={12} sm={9} md={10} lg={11} />
+    </Row>
+    <Row>
+      <Box xs={10} sm={6} md={8} lg={10} />
+      <Box xs={2} sm={6} md={4} lg={2} />
+    </Row>
+    <Row>
+      <Markdown html = {responsiveMD} />
+    </Row>
+  </Grid>
   </article>
 );
 
