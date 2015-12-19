5a8c37074cf33ce31051433e74dccfc95358d167
roylee0704 <roylee0704@gmail.com>
2015-12-19 17:07:52 +0800

    added new segment: alignment: start

--- doc/app/components/home/index.jsx
+++ doc/app/components/home/index.jsx
@@ -10,6 +10,7 @@ import responsiveMD from './responsive';
 import fluidMD from './fluid';
 import offsetMD from './offset';
 import autoWidthMD from './auto-width';
+import alignStartMD from './align-start';
 
 
 const Home = () => (
@@ -19,7 +20,7 @@ const Home = () => (
         <h1 className={home.headline}>React-FlexBox-Grid</h1>
       </Row>
       <Row center="xs">
-        <p></p>
+        <p>A react grid system based on flexboxgrid.css</p>
       </Row>
       <Row center="xs">
         <a href="https://github.com/roylee0704/react-flexbox-grid">Github</a>
@@ -30,17 +31,17 @@ const Home = () => (
         title="Responsive"
         description="Responsive modifiers enable specifying different column sizes, offsets, alignment and distribution at xs, sm, md & lg viewport widths.">
         <Row>
-          <Box xs={12} sm={3} md={2} lg={1} />
-          <Box xs={6} sm={6} md={8} lg={10} />
-          <Box xs={6} sm={3} md={2} lg={1} />
+          <Box type="row" xs={12} sm={3} md={2} lg={1} />
+          <Box type="row" xs={6} sm={6} md={8} lg={10} />
+          <Box type="row" xs={6} sm={3} md={2} lg={1} />
         </Row>
         <Row>
-          <Box xs={12} sm={3} md={2} lg={1} />
-          <Box xs={12} sm={9} md={10} lg={11} />
+          <Box type="row" xs={12} sm={3} md={2} lg={1} />
+          <Box type="row" xs={12} sm={9} md={10} lg={11} />
         </Row>
         <Row>
-          <Box xs={10} sm={6} md={8} lg={10} />
-          <Box xs={2} sm={6} md={4} lg={2} />
+          <Box type="row" xs={10} sm={6} md={8} lg={10} />
+          <Box type="row" xs={2} sm={6} md={4} lg={2} />
         </Row>
         <Row>
           <Markdown html = {responsiveMD} />
@@ -48,17 +49,17 @@ const Home = () => (
       </Section>
       <Section title = "Fluid" description = "Percent based width allow fluid resizing of columns and rows.">
         <Row>
-          <Box xs={12} sm={3} md={2} lg={1} />
-          <Box xs={6} sm={6} md={8} lg={10} />
-          <Box xs={6} sm={3} md={2} lg={1} />
+          <Box type="row" xs={12} sm={3} md={2} lg={1} />
+          <Box type="row" xs={6} sm={6} md={8} lg={10} />
+          <Box type="row" xs={6} sm={3} md={2} lg={1} />
         </Row>
         <Row>
-          <Box xs={12} sm={3} md={2} lg={1} />
-          <Box xs={12} sm={9} md={10} lg={11} />
+          <Box type="row" xs={12} sm={3} md={2} lg={1} />
+          <Box type="row" xs={12} sm={9} md={10} lg={11} />
         </Row>
         <Row>
-          <Box xs={10} sm={6} md={8} lg={10} />
-          <Box xs={2} sm={6} md={4} lg={2} />
+          <Box type="row" xs={10} sm={6} md={8} lg={10} />
+          <Box type="row" xs={2} sm={6} md={4} lg={2} />
         </Row>
         <Row>
           <Markdown html = {fluidMD} />
@@ -66,17 +67,17 @@ const Home = () => (
       </Section>
       <Section title = "Offsets" description = "Offset a column.">
         <Row>
-          <Box xsOffset={11} xs={1} />
-          <Box xsOffset={10} xs={2} />
-          <Box xsOffset={9} xs={3} />
-          <Box xsOffset={8} xs={4} />
-          <Box xsOffset={7} xs={5} />
-          <Box xsOffset={6} xs={6} />
-          <Box xsOffset={5} xs={7} />
-          <Box xsOffset={4} xs={8} />
-          <Box xsOffset={3} xs={9} />
-          <Box xsOffset={2} xs={10} />
-          <Box xsOffset={1} xs={11} />
+          <Box type="row" xsOffset={11} xs={1} />
+          <Box type="row" xsOffset={10} xs={2} />
+          <Box type="row" xsOffset={9} xs={3} />
+          <Box type="row" xsOffset={8} xs={4} />
+          <Box type="row" xsOffset={7} xs={5} />
+          <Box type="row" xsOffset={6} xs={6} />
+          <Box type="row" xsOffset={5} xs={7} />
+          <Box type="row" xsOffset={4} xs={8} />
+          <Box type="row" xsOffset={3} xs={9} />
+          <Box type="row" xsOffset={2} xs={10} />
+          <Box type="row" xsOffset={1} xs={11} />
         </Row>
         <Row>
           <Markdown html = {offsetMD} />
@@ -84,16 +85,29 @@ const Home = () => (
       </Section>
       <Section title = "Auto Width" description = "Add any number of auto sizing columns to a row. Let the grid figure it out.">
         <Row>
-          <Box xs />
-          <Box xs />
+          <Box type="row" xs />
+          <Box type="row" xs />
         </Row>
         <Row>
-          <Box xs />
-          <Box xs />
-          <Box xs />
+          <Box type="row" xs />
+          <Box type="row" xs />
+          <Box type="row" xs />
         </Row>
         <Row>
           <Markdown html = {autoWidthMD} />
+        </Row>
+      </Section>
+      <Section title = "Alignment" description = "Add classes to align elements to the start or end of row as well as the top, bottom, or center of a column">
+        <h3><code>.start-</code></h3>
+        <Row>
+          <Box type="container" xs={12}>
+            <Row start="xs">
+              <Box type="nested" xs={6} />
+            </Row>
+          </Box>
+        </Row>
+        <Row>
+          <Markdown html = {alignStartMD} />
         </Row>
       </Section>
     </Grid>
