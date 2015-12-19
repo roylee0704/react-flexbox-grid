6479589d228867b04d1e004bc003edbf51534dc6
roylee0704 <roylee0704@gmail.com>
2015-12-19 17:06:50 +0800

    added box-type props and styles

--- doc/app/components/box/index.jsx
+++ doc/app/components/box/index.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, {PropTypes} from 'react';
 import {Col} from 'react-flexbox-grid';
 import box from './style';
 
 const Box = (props) => {
   return (
     <Col {...props}>
-        <div className={box.row} />
+        <div className = {box[props.type]}>
+          {props.children}
+        </div>
     </Col>
   );
+};
+
+Box.propTypes = {
+  type: PropTypes.oneOf(['row', 'container', 'nested']).isRequired,
+  children: PropTypes.node
 };
 
 export default Box;
