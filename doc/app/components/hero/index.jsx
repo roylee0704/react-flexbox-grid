3083e655c1518f420b2f6868eccc1a9f3982423b
roylee0704 <roylee0704@gmail.com>
2015-12-20 23:00:10 +0800

    decomposed to hero component

---
+++ doc/app/components/hero/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Row} from 'react-flexbox-grid';
+import style from './style';
+
+const Hero = () => {
+  return (
+    <header>
+      <Row center="xs">
+        <h1 className={style.headline}>React-FlexBox-Grid</h1>
+      </Row>
+      <Row className={style.description} center="xs">
+        <code>React(CSS-Modules(flexboxgrid.css));</code>
+      </Row>
+      <Row center="xs">
+        <a href="https://github.com/roylee0704/react-flexbox-grid">Github</a>
+      </Row>
+    </header>
+  );
+};
+
+export default Hero;
