9a225dbba109e84c8c83d28babace8fc8e3388de
Ivan Tugay <listepo@gmail.com>
2016-02-12 15:08:09 +0200

    Fix https://github.com/roylee0704/react-flexbox-grid/issues/17

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,6 +1,6 @@
 import React, {Component, PropTypes} from 'react';
 import classNames from 'classnames';
-import style from '../style';
+import style from '../style.scss';
 
 export default class Grid extends Component {
   render() {
