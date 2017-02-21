92ab2dd7f198b567b3075de572b393f2f6aee874
Nathan Stitt <nathan@stitt.org>
2017-02-21 11:42:51 -0600

    remove unused imports

--- test/classNames.spec.js
+++ test/classNames.spec.js
@@ -1,11 +1,6 @@
 import expect from 'expect';
-import React from 'react';
 import style from 'flexboxgrid';
-import TestUtils from 'react-addons-test-utils';
-import Col from '../src/components/Col';
-import getClass, { setClassNameLookup } from '../src/classNames';
-
-const renderer = TestUtils.createRenderer();
+import getClass from '../src/classNames';
 
 describe('classNames lookups', () => {
   it('translates known styles', () => {
