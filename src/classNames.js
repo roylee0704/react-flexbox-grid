8cc65537a3c471db2e772dcdea738ddfcfd24dbb
Nathan Stitt <nathan@stitt.org>
2017-02-27 04:19:25 -0600

    Add extension onto flexboxgrid import (#93)
    Makes it easier to mock when testing, since testing setups have a role saying to mock out '*.css'

--- src/classNames.js
+++ src/classNames.js
@@ -1,4 +1,4 @@
-import styles from 'flexboxgrid';
+import styles from 'flexboxgrid/dist/flexboxgrid.css';
 
 export default function getClass(className) {
   return (styles && styles[className]) ? styles[className] : className;
